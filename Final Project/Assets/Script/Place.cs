e45cd7a37e7151c6cc28c72042201753d7a7a098
bohecmian <bohecmian@gmail.com>
2024-12-07 12:59:44 -0500

    collision continue

--- Final Project/Assets/Script/Place.cs
+++ Final Project/Assets/Script/Place.cs
@@ -7,18 +7,24 @@ public class Place : MonoBehaviour
     // Start is called before the first frame update
     public GameObject Placeobject;
     public GameObject finishobject;
+    public GameObject nextobject;
+    public GameObject currentUI;
+    public GameObject nextUI;
     void Start()
     {
-        
+        finishobject.SetActive(false);
     }
 
     // Update is called once per frame
-    void OntriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if(other.gameObject.tag == "place")
         {
-            Destroy(Placeobject);
+            Destroy(gameObject);
             finishobject.SetActive(true);
+            nextobject.SetActive(true);
+            currentUI.SetActive(false);
+            nextUI.SetActive(true);
 
         }
     }
